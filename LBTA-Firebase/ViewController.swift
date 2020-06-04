78532fc932c5c121dd21006bc601cfe6e6b8013a
Dominique Michael Abejar <35872556+AinScruff@users.noreply.github.com>
2020-06-04 22:21:29 +0800

    Initial xcode Project

---
+++ LBTA-Firebase/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  LBTA-Firebase
+//
+//  Created by Dominique Michael Abejar on 6/4/20.
+//  Copyright © 2020 NinjaTuna. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
