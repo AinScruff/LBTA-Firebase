b1d5668f744d3d201d0f97cc25b9b34fb19977c7
Dominique Michael Abejar <35872556+AinScruff@users.noreply.github.com>
2020-06-17 01:27:24 +0800

    Initial Utilities class

---
+++ LBTA-Firebase/Utilities/Utilities.swift
@@ -0,0 +1,30 @@
+//
+//  Utilities.swift
+//  LBTA-Firebase
+//
+//  Created by Dominique Michael Abejar on 6/16/20.
+//  Copyright © 2020 NinjaTuna. All rights reserved.
+//
+
+import UIKit
+
+class Utilities {
+
+    static func isEmailValid(_ email: String) -> Bool {
+        let emailRegex = NSPredicate(format: "SELF MATCHES %@", "(?:[\\p{L}0-9!#$%\\&'*+/=?\\^_`{|}~-]+(?:\\.[\\p{L}0-9!#$%\\&'*+/=?\\^_`{|}" +
+        "~-]+)*|\"(?:[\\x01-\\x08\\x0b\\x0c\\x0e-\\x1f\\x21\\x23-\\x5b\\x5d-\\" +
+        "x7f]|\\\\[\\x01-\\x09\\x0b\\x0c\\x0e-\\x7f])*\")@(?:(?:[\\p{L}0-9](?:[a-" +
+        "z0-9-]*[\\p{L}0-9])?\\.)+[\\p{L}0-9](?:[\\p{L}0-9-]*[\\p{L}0-9])?|\\[(?:(?:25[0-5" +
+        "]|2[0-4][0-9]|[01]?[0-9][0-9]?)\\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-" +
+        "9][0-9]?|[\\p{L}0-9-]*[\\p{L}0-9]:(?:[\\x01-\\x08\\x0b\\x0c\\x0e-\\x1f\\x21" +
+        "-\\x5a\\x53-\\x7f]|\\\\[\\x01-\\x09\\x0b\\x0c\\x0e-\\x7f])+)\\])")
+        
+        return emailRegex.evaluate(with: email)
+    }
+    
+    static func isPasswordValid(_ password: String) -> Bool {
+        let passwordRegex = NSPredicate(format: "SELF MATCHES %@", "^(?=.*[A-Z])(?=.*[a-z])(?=.*[0-9]).{6,}$")
+
+        return passwordRegex.evaluate(with: password)
+    }
+}
