ddf5399a9fc5a23aa8ac83a5704d75d9ce4013cc
Dominique Michael Abejar <35872556+AinScruff@users.noreply.github.com>
2020-08-04 00:18:16 +0800

    Created ChatViewController & TabBarViewController

---
+++ LBTA-Firebase/Controller/Chat/ChatViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ChatViewController.swift
+//  LBTA-Firebase
+//
+//  Created by Dominique Michael Abejar on 8/3/20.
+//  Copyright © 2020 NinjaTuna. All rights reserved.
+//
+
+import UIKit
+
+class ChatViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        view.backgroundColor = .systemRed
+    }
+    
+
+}
---
+++ LBTA-Firebase/Controller/Main View /TabBarViewController.swift
@@ -0,0 +1,37 @@
+//
+//  TabBarViewController.swift
+//  LBTA-Firebase
+//
+//  Created by Dominique Michael Abejar on 8/3/20.
+//  Copyright © 2020 NinjaTuna. All rights reserved.
+//
+
+import UIKit
+
+class TabBarViewController: UITabBarController {
+    
+    // MARK: - LifeCycle
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        let vc1 = createTabBarItem(tabBarTitle: "Chat", vc: ChatViewController(), tabBarImage: "chat")
+        let vc2 = createTabBarItem(tabBarTitle: "Profile", vc: ChatViewController(), tabBarImage: "user")
+        viewControllers = [vc1, vc2]
+    }
+    
+  
+}
+
+// MARK: - Private Methods
+extension TabBarViewController {
+    
+    private func createTabBarItem(tabBarTitle: String, vc: UIViewController, tabBarImage: String) -> UINavigationController{
+        
+        let navCont = UINavigationController(rootViewController: vc)
+        navCont.tabBarItem.title = tabBarTitle
+        navCont.tabBarItem.image = UIImage(named: tabBarImage)
+        
+        return navCont
+    }
+}
