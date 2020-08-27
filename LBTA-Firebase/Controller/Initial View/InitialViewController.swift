afbd613f2e3f77f7e9f97cd5c2ab3a327761944d
Dominique Michael Abejar <35872556+AinScruff@users.noreply.github.com>
2020-08-28 01:40:56 +0800

    Changed Initial View from Navigation Controller to UIVIewController & Added AutoLogin using Keychain

--- LBTA-Firebase/Controller/Initial View/InitialViewController.swift
+++ LBTA-Firebase/Controller/Initial View/InitialViewController.swift
@@ -8,22 +8,51 @@
 
 import UIKit
 import Firebase
+import KeychainSwift
 
-class InitialViewController: UINavigationController {
+class InitialViewController: UIViewController {
 
     // MARK: - Properties
-    var vc = UIViewController()
-    let validation = LogInValidationService.shared
     
+    let validation = LogInValidationService.shared
+    let services = ServicesAPI.shared
+    let keyRef = Constants.Keys.KEYCHAIN_REF
+      
+    let emailKey = Constants.Keys.EMAIL
+    let passwordKey = Constants.Keys.PASSWORD
+  
     // MARK: - Lifecycle
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        view.backgroundColor = .systemBackground
+        view.backgroundColor = .systemBlue
+       
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+       if let email = keyRef.get(emailKey), let password = keyRef.get(passwordKey) {
+           login(email, password)
+        } else {
+            let vc = LogInViewController(validation: validation)
+            vc.modalPresentationStyle = .fullScreen
+            self.present(vc, animated: false)
+        }
         
-        // Log in user using keychain
-        viewControllers = [LogInViewController(validation: validation)]
     }
     
     // MARK: - Methods
+    
+    private func login(_ email: String, _ password: String) {
+        
+        Auth.auth().signIn(withEmail: email, password: password) { (result, error) in
+            
+            if error != nil {
+                print("Something went Wrong")
+            } else {
+                let vc = TabBarViewController()
+                vc.modalPresentationStyle = .fullScreen
+                self.present(vc, animated: false)
+            }
+        }
+    }
 }
