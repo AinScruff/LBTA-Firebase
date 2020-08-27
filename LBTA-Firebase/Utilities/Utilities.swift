875ebcd377d3d9204c7eebe3b9ef9ce341f64f48
Dominique Michael Abejar <35872556+AinScruff@users.noreply.github.com>
2020-08-27 23:03:50 +0800

    Removed validation methods transferred to validation services

--- LBTA-Firebase/Utilities/Utilities.swift
+++ LBTA-Firebase/Utilities/Utilities.swift
@@ -9,27 +9,6 @@
 import UIKit
 
 class Utilities {
-
-    // Check Email Format if Valid
-    static func isEmailValid(_ email: String) -> Bool {
-        let emailRegex = NSPredicate(format: "SELF MATCHES %@", "(?:[\\p{L}0-9!#$%\\&'*+/=?\\^_`{|}~-]+(?:\\.[\\p{L}0-9!#$%\\&'*+/=?\\^_`{|}" +
-        "~-]+)*|\"(?:[\\x01-\\x08\\x0b\\x0c\\x0e-\\x1f\\x21\\x23-\\x5b\\x5d-\\" +
-        "x7f]|\\\\[\\x01-\\x09\\x0b\\x0c\\x0e-\\x7f])*\")@(?:(?:[\\p{L}0-9](?:[a-" +
-        "z0-9-]*[\\p{L}0-9])?\\.)+[\\p{L}0-9](?:[\\p{L}0-9-]*[\\p{L}0-9])?|\\[(?:(?:25[0-5" +
-        "]|2[0-4][0-9]|[01]?[0-9][0-9]?)\\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-" +
-        "9][0-9]?|[\\p{L}0-9-]*[\\p{L}0-9]:(?:[\\x01-\\x08\\x0b\\x0c\\x0e-\\x1f\\x21" +
-        "-\\x5a\\x53-\\x7f]|\\\\[\\x01-\\x09\\x0b\\x0c\\x0e-\\x7f])+)\\])")
-        
-        return emailRegex.evaluate(with: email)
-    }
-    
-    // Check Password Regex 
-    static func isPasswordValid(_ password: String) -> Bool {
-        let passwordRegex = NSPredicate(format: "SELF MATCHES %@", "^(?=.*[A-Z])(?=.*[0-9]).{6,}$")
-
-        return passwordRegex.evaluate(with: password)
-    }
-    
     
     // Show Error Message
     static func showErrorView(title: String, message: String) -> UIViewController {
@@ -39,7 +18,7 @@ class Utilities {
         vc.modalTransitionStyle = .crossDissolve
         vc.titleMessage = title
         vc.errorMessage = message
-        
+    
         return vc
     }
     
