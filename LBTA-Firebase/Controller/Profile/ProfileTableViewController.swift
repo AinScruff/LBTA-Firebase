4b751e31ea7893588563d1bdf8ed8003e1c2d0bf
Dominique Michael Abejar <35872556+AinScruff@users.noreply.github.com>
2020-09-02 00:42:45 +0800

    Registered Profile Header Cell and Added User Cache

--- LBTA-Firebase/Controller/Profile/ProfileTableViewController.swift
+++ LBTA-Firebase/Controller/Profile/ProfileTableViewController.swift
@@ -13,45 +13,111 @@ import KeychainSwift
 class ProfileTableViewController: UITableViewController {
     
     // MARK: - Properties
+    let defaults = UserDefaults.standard
     let keychain = Constants.Keys.KEYCHAIN_REF
     let auth = Constants.API.AUTH_REF
     
-    let cellId = "cell"
-    
+    var user = User()
+    var options = ["My Profile", "Sign Out"]
     // MARK: - View Elements
     
+    let headerCellID = "headerCell"
+    let cellId = "cell"
+    
+    fileprivate let tableFooter: UIView = {
+        let tf = UIView(frame: .zero)
+        
+        tf.backgroundColor = .white
+        
+        return tf
+    }()
+    
+    // MARK: - Initialization
+    
+    let services: UserService
+    
+    init(services: UserService) {
+        self.services = services
+        super.init(nibName: nil, bundle: nil)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
     // MARK: - Lifecycle
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
         configureTableView()
+        getUser()
     }
     
-    // MARK: - UITableView DataSource
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        
+        tableView.tableFooterView = tableFooter
+    }
+    
+}
+
+// MARK: - Constraints
+extension ProfileTableViewController {
+    
+    fileprivate func configureTableView() {
+        navigationItem.title = "Profile"
+        registerCells()
+    }
+    
+    fileprivate func registerCells() {
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: cellId)
+        tableView.register(ProfileHeaderTableViewCell.self, forCellReuseIdentifier: headerCellID)
+    }
+}
+
+// MARK: - UITableView DataSource
+extension ProfileTableViewController {
     
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 3
+        return options.count + 1
     }
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         
-        let cell = tableView.dequeueReusableCell(withIdentifier: cellId, for: indexPath)
+        if indexPath.row == 0 {
+            let cell = tableView.dequeueReusableCell(withIdentifier: headerCellID, for: indexPath) as! ProfileHeaderTableViewCell
+            
+            cell.populateCell(user: user)
+            cell.isUserInteractionEnabled = false
+            
+            return cell
+        } else {
+            let cell = tableView.dequeueReusableCell(withIdentifier: cellId, for: indexPath)
+            
+            
+            cell.textLabel?.text = options[indexPath.row - 1]
+           
+            return cell
+        }
         
-        cell.textLabel?.text = "aDIUAHSFIUHAS"
-        
-        return cell
     }
     
-    // MARK: - UITableView Delegate
+}
+
+// MARK: - UITableView Delegate
+extension ProfileTableViewController {
     
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        
+        tableView.deselectRow(at: indexPath, animated: true)
         
         if indexPath.row == 2 {
             
             let signOutAction = UIAlertAction(title: "Sign Out", style: .destructive) { (action) in
                 do {
                     try self.auth.signOut()
-                    self.keychain.clear()
+                    UserProfileCache.remove()
                     self.view.window?.rootViewController?.dismiss(animated: false, completion: nil)
                 } catch {
                     print("Err")
@@ -61,11 +127,19 @@ class ProfileTableViewController: UITableViewController {
             let cancelAction = UIAlertAction(title: "Cancel", style: .cancel, handler: nil)
             
             showErrorView(actions: [signOutAction, cancelAction])
-            
         }
     }
     
-    // MARK: - Methods
+    override func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        if indexPath.row == 0 {
+            return 250
+        } else {
+            return UITableView.automaticDimension
+        }
+    }
+}
+// MARK: - Methods
+extension ProfileTableViewController {
     
     fileprivate func showErrorView(actions: [UIAlertAction]) {
         let alert = UIAlertController(title: "Sign Out", message: nil, preferredStyle: .actionSheet)
@@ -76,16 +150,21 @@ class ProfileTableViewController: UITableViewController {
         
         self.present(alert, animated: true, completion: nil)
     }
-}
-
-// MARK: - Constraints
-extension ProfileTableViewController {
     
-    fileprivate func configureTableView() {
-        registerCells()
-    }
-    
-    fileprivate func registerCells() {
-        tableView.register(UITableViewCell.self, forCellReuseIdentifier: cellId)
+    fileprivate func getUser() {
+            
+        if let uid = Constants.API.AUTH_REF.currentUser?.uid {
+            if let savedUser = UserProfileCache.get() {
+                user = savedUser
+            } else {
+                services.fetchCurrentUserData(userID: uid) { user in
+                    self.user = user
+                    self.tableView.reloadData()
+                }
+            }
+        } else {
+            self.view.window?.rootViewController?.dismiss(animated: false, completion: nil)
+        }
     }
 }
+// ADD LISTENER IN CASE IF UPDATE USER
