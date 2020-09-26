e9bcc1b775359e5dd944cedff8dc58351f4eefd0
Dominique Michael Abejar <35872556+AinScruff@users.noreply.github.com>
2020-09-26 15:55:15 +0800

    Added dateLabel, checkDate and isSameDate functions and modified populateCell

--- LBTA-Firebase/View/Chat/UserChatTableViewCell.swift
+++ LBTA-Firebase/View/Chat/UserChatTableViewCell.swift
@@ -41,7 +41,15 @@ class UserChatTableViewCell: UITableViewCell {
         return cl
     }()
     
-   
+    fileprivate var dateLabel: UILabel = {
+        var dl = UILabel()
+        
+        dl.font = UIFont(name: "Helvetica", size: 13)
+        dl.textColor = .systemGray2
+        dl.numberOfLines = 0
+        
+        return dl
+    }()
     // MARK: - Initialization
     
     override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
@@ -73,26 +81,65 @@ extension UserChatTableViewCell {
         stackView.spacing = .zero
         
         addSubview(stackView)
+        addSubview(dateLabel)
         addSubview(profileImageView)
-        
+      
         profileImageView.anchor(top: topAnchor, leading: leadingAnchor, bottom: bottomAnchor, paddingTop: 5, paddingLeading: 5, paddingBottom: 5, width: 50)
         
-        stackView.anchor(top: topAnchor, leading: profileImageView.trailingAnchor, bottom: bottomAnchor, trailing: trailingAnchor, paddingTop: 5, paddingLeading: 5, paddingBottom: 5, paddingTrailing: 5)
+        dateLabel.anchor(top: topAnchor, bottom: bottomAnchor, trailing: trailingAnchor, paddingTrailing: 5, width: 50)
+        
+        stackView.anchor(top: topAnchor, leading: profileImageView.trailingAnchor, bottom: bottomAnchor, trailing: dateLabel.leadingAnchor, paddingTop: 5, paddingLeading: 5, paddingBottom: 5)
     }
     
 }
 // MARK: - Methods
 extension UserChatTableViewCell {
     
-    func populateCell(user: User) {
+    func populateCell(with userMessage: UserMessage) {
         
-        if let imageURL = user.imageURL {
+        if let imageURL = userMessage.user.imageURL {
             ImageService.getImage(withURL: imageURL) { image in
                 self.profileImageView.image = image
             }
         }
         
-        self.nameLabel.text = user.name
-        self.chatLabel.text = "ASDAHISDADS"
+        self.nameLabel.text = userMessage.user.name
+        
+        if let latestMsg  = userMessage.latestMessage {
+            self.chatLabel.text = latestMsg.message
+            self.dateLabel.text = checkDate(latestMsg.dateSent)
+        } else {
+            self.chatLabel.text = "Chat with your friend now!"
+        }
+            
     }
+    
+
+    fileprivate func checkDate(_ date: Date) -> String {
+        
+        if isSameDate(date) {
+            return date.getTime()
+        } else {
+            let formatter = DateFormatter()
+            
+            formatter.dateStyle = .short
+
+            return formatter.string(from: date)
+        }
+    }
+
+    fileprivate func isSameDate(_ date: Date) -> Bool {
+        let today = Date()
+
+        let diff = Calendar.current.dateComponents([.month], from: date, to: today)
+        
+        return diff.day == 0 ? true : false
+    }
+
 }
+
+
+
+// Convert date to String
+// convert date to local time
+// Compare date == today show time else show date
